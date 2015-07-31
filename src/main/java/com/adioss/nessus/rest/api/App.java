da2f5489d3cf7dd0ec8e97930706d96af65f0d3f
unknown <adio@ITEM-AX41448.axway.int>
2015-07-31 17:04:34 +0200

    First implementation (two examples: list scans +get session token)

--- src/main/java/com/adioss/nessus/rest/api/App.java
+++ src/main/java/com/adioss/nessus/rest/api/App.java
@@ -21,7 +21,7 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class App {
-    public static final String NESSUS_URL = "https://url:port";
+    public static final String NESSUS_URL = "http://url:port";
     public static final String USERNAME = "USERNAME";
     public static final String PASSWORD = "PASSWORD";
     public static final String ACCESS_KEY = "ACCESS_KEY";
